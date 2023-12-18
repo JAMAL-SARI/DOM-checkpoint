29ebe9202b9162cc4b06213259a6385eecf06883
JAMAL-SARI <geologue.dahbaoui@gmail.com>
2023-12-18 21:18:35 +0100

    version fatal

--- main.js
+++ main.js
@@ -1,86 +1,80 @@
-let prixUnit   =document.querySelectorAll('#prixUnit');
-let prixTota   =document.querySelectorAll('#prixTota');
-let discre =document.querySelectorAll('.discre');
-let crem   =document.querySelectorAll('.crem');
-let suprimer =document.querySelectorAll('.supremer');
-let aimer   =document.querySelectorAll('.aimer');
-let coeur   =document.querySelectorAll('.coeur');
-let number  = document.querySelectorAll("#number");
-let articl=document.querySelectorAll(".articl");
-console.log(aimer);
+let prixUnit   =document.querySelector('#prixUnit');
+let prixTota   =document.querySelector('#prixTota');
+let discre =document.querySelector('.discre');
+let crem   =document.querySelector('.crem');
+let suprimer =document.querySelector('.suprimer');
+let aimer   =document.querySelector('.aimer');
+let coeur   =document.querySelector('#coeur');
+let number  = document.querySelector("#number");
+let articl=document.querySelector(".articl");
+
+console.log(coeur);
+
+
+
 
 for(let i=0;i<suprimer.length;i++){
     
     suprimer[i].addEventListener("click",function()
     {
-    this.parentNode.remove()
+    suprimer[i].parentNode.remove()
     });
 }
+
 
 for(let j=0;j<aimer.length;j++){
-    
+ 
+
     aimer[j].addEventListener("click",function()
     {
-    this.style.color='red';
+        // console.log(aimer[j].outerHTML);
+     aimer[j].style.background='red';
+    } );
+}
+
+
+
+// console.log(indexOff(this.suprimer));
+for(i=0; i<suprimer.length ; i++){
+    suprimer[i].addEventListener("click",function(){
+        // alert(this.innerText + "clicked");
+    console.log(suprimer[i]);
     });
-}
-
 
     
-   
-
-
-
-
-
-
-
-
-// // console.log(indexOff(this.suprimer));
-// for(i=0; i<suprimer.length ; i++){
-//     suprimer[i].addEventListener("click",function(){
-//         // alert(this.innerText + "clicked");
-//     console.log(suprimer[i]);
-//     });
-// //   suprimer.onclick=function(){
+//   suprimer.onclick=function(){
     
-// //  i = suprimer[i] }
-// //  console.log(i);}
+//  i = suprimer[i] }
+//  console.log(i);}
 
 
 
 
 
 
+////////////////////////////////////
+// ///////////////////////////////////////
+// code pour un suel  produit 
 
+// discre.onclick=function(){
+//     number.value--
+//     if(number.value<1){
+//        number.value=1
+       
+//     }  
 
-
-
-
-
-
-discre.onclick=function(){
-    number.value--
-    if(number.value<1){
-       number.value=1
-    }
-   
-    prixTota.value=prixUnit.value*number.value;
+//     prixTota.value=prixUnit.value*number.value;
   
+// }
+// crem.onclick=function(){
+//     number.value++
 
-}
-crem.onclick=function(){
-    number.value++
    
-   
-    prixTota.value=prixUnit.value*number.value;
-}
+//     prixTota.value=prixUnit.value*number.value;
+// }
 
 aimer.onclick=function(){
+    prixTota.style.color="red"
     coeur.style.color='red';
 }
-// suprimer.onclick=function(){
-//     articl.nodeList[4].remove();
-    
-//     alert("are you sur the delet this prodact")}
 
